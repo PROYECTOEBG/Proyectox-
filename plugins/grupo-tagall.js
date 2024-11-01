152f2c7000cdeb6a788c0cd1a7d1daf9a45399ad
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-01 03:17:13 -0500

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -1,20 +1,21 @@
-let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
-if (!(isAdmin || isOwner)) {
-global.dfail('admin', m, conn)
-throw false
-}
-let pesan = args.join` `
-let oi = `ღ ${lenguajeGB['smsAddB5']()} ${pesan}`
-let teks = `╭━〔 *${lenguajeGB['smstagaa']()}* 〕━⬣\n\n${oi}\n\n`
-for (let mem of participants) {
-teks += `┃⊹ @${mem.id.split('@')[0]}\n`}
-teks += `┃\n`
-teks += `┃ ${wm}\n`
-teks += `╰━━━━━[ *𓃠 ${vs}* ]━━━━━⬣`
-conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
-}
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
-export default handler
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+  if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn);
+    throw false;
+  }
+  const pesan = args.join` `;
+  const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
+  let teks = `⺀𝐋 𝐋 𝐀 𝐌 𝐀 𝐍 𝐃 𝐎 - 𝐆 𝐑 𝐔 𝐏 𝐎⺀\n\n➪ ${oi}\n\n➪ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  for (const mem of participants) {
+    teks += `┣☢︎︎ @${mem.id.split('@')[0]}\n`;
+  }
+  teks += `└ 𝑳𝑶𝑩𝑶-𝑩𝑶𝑻-𝑴𝑫\n\n▌│█║▌║▌║║▌║▌║▌║█`;
+  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+};
+handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
+handler.tags = ['group'];
+handler.command = /^(tagall|invocar|llamar|todos|vengan)$/i;
+handler.admin = true;
+handler.group = true;
+export default handler;
