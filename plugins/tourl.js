80968350fd3c3162306bd69adb48a68ca8d275b3
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-10 10:45:21 -0500

    Update tourl.js

--- plugins/tourl.js
+++ plugins/tourl.js
@@ -1,21 +1,28 @@
-import uploadFile from '../../lib/uploadFile.js'
-import uploadImage from '../../lib/uploadImage.js'
-
+/* 
+# Créditos a https://github.com/AzamiJs
+•• @AzamiJs ••
+*/
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import fetch from 'node-fetch'
 let handler = async (m) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw '⚠️️ *_Responde a una imagen/video._*'
-  let media = await q.download()
-  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-  let link = await (isTele ? uploadImage : uploadFile)(media)
-  m.reply(`𝗣𝗘𝗦𝗢 𝗗𝗘𝗟 𝗔𝗥𝗖𝗛𝗜𝗩𝗢 📁\n${media.length} Byte(s) 
-
-⭐ ${isTele ? '𝖲𝖨𝖭 𝖥𝖤𝖢𝖧𝖠 𝖣𝖤 𝖢𝖠𝖣𝖴𝖢𝖨𝖣𝖠𝖣' : '(Desconocido)'} 
-🌎 𝖫𝖨𝖭𝖪:\n${link}
-  `)
-}
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => gataImg)
+let name = await conn.getName(who)
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw `${mg} ${mid.smsconvert10}`
+let media = await q.download()
+let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+let link = await (isTele ? uploadImage : uploadFile)(media)
+let caption = `🛑 ${mid.smsYT4}:\n${link}\n🥏 ${mid.smsconvert11}: ${media.length}\n🚀 ${mid.smsconvert12}: ${isTele ? '𝙉𝙊 𝙀𝙓𝙋𝙄𝙍𝘼' : '𝘿𝙀𝙎𝘾𝙊𝙉𝙊𝘾𝙄𝘿𝙊'}\n🔰 ${mid.smsconvert13}: ${await shortUrl(link)}`
+conn.reply(m.chat, caption, m, { contextInfo: {externalAdReply :{mediaUrl: md, mediaType: 2, title: wm, body: botdate, thumbnail: await(await fetch(link)).buffer(), sourceUrl: link }}})}
 handler.help = ['tourl']
-handler.tags = ['tools']
-handler.command = ['upload', 'tourl']
-
+handler.tags = ['herramientas']
+handler.command = /^(tourl|upload)$/i
 export default handler
+
+async function shortUrl(url) {
+let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
+return await res.text()
+}
