c26574f40657edb50e1478b79089ebba309e8a51
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-14 08:49:29 -0500

    Update 111.js

--- plugins/111.js
+++ plugins/111.js
@@ -1,41 +1,26 @@
-import util from 'util'
-import path from 'path'
-let user = a => '@' + a.split('@')[0]
-function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
-if (!text) return conn.reply(m.chat, 'Ejemplo de uso: #top *texto*', m, rcanal)
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b = ps.getRandom()
-let c = ps.getRandom()
-let d = ps.getRandom()
-let e = ps.getRandom()
-let f = ps.getRandom()
-let g = ps.getRandom()
-let h = ps.getRandom()
-let i = ps.getRandom()
-let j = ps.getRandom()
-let k = Math.floor(Math.random() * 70);
-let x = `${pickRandom(['🤓','😅','😂','😳','😎', '🥵', '😱', '🤑', '🙄', '💩','🍑','🤨','🥴','🔥','👇🏻','😔', '👀','🌚'])}`
-let l = Math.floor(Math.random() * x.length);
-let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
-let top = `*${x} Top 10 ${text} ${x}*
-    
-*1. ${user(a)}*
-*2. ${user(b)}*
-*3. ${user(c)}*
-*4. ${user(d)}*
-*5. ${user(e)}*
-*6. ${user(f)}*
-*7. ${user(g)}*
-*8. ${user(h)}*
-*9. ${user(i)}*
-*10. ${user(j)}*`
-m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]})
-}
-handler.help = ['top *<texto>*']
-handler.command = ['topp']
-handler.tags = ['fun']
-handler.group = true
-export default handler
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]
+const xpperestrellas = 350;
+const handler = async (m, {conn, command, args}) => {
+  let count = command.replace(/^buy/i, '');
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperestrellas) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  count = Math.max(1, count);
+  if (global.db.data.users[m.sender].exp >= xpperestrellas * count) {
+    global.db.data.users[m.sender].exp -= xpperestrellas * count;
+    global.db.data.users[m.sender].estrellas += count;
+    conn.reply(m.chat, `
+╔═══════⩽✰⩾═══════╗
+║    𝐍𝐨𝐭𝐚 𝐃𝐞 𝐏𝐚𝐠𝐨 
+╠═══════⩽✰⩾═══════╝
+║╭──────────────┄
+║│ *Compra Nominal* : + ${count}🌟
+║│ *Gastado* : -${xpperestrellas * count} XP
+║╰──────────────┄
+╚═══════⩽✰⩾═══════╝`, m);
+  } else conn.reply(m.chat, `😔 Lo siento, no tienes suficiente *XP* para comprar *${count}* Estrellas 🌟`, m, rcanal);
+};
+handler.help = ['Buy', 'Buyall'];
+handler.tags = ['xp'];
+handler.command = ['buuy', 'buyall'];
+
+handler.disabled = false;
+
+export default handler;
