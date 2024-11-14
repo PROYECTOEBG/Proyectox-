3acb9873a8e749ef399cf572ff11f7eee5dd95ca
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-14 08:37:24 -0500

    Update Tiktok.js

--- plugins/Tiktok.js
+++ plugins/Tiktok.js
@@ -86,7 +86,7 @@ let category = "imagen"
 //const random = Math.floor(Math.random() * db_.links[category].length)
 //const randomlink = db_.links[category][random]
 //const response = await fetch(randomlink)
-const rimg = await response.buffer()
+//const rimg = await response.buffer()
 global.icons = rimg
 
 //• ↳ ◜𝑻𝑰𝑬𝑴𝑷𝑶 𝑹𝑷𝑮◞ • ⚔
