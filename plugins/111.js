057ef4e6e0082391be53d865c5161d3f0900f7af
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-14 19:44:44 -0500

    Update 111.js

--- plugins/111.js
+++ plugins/111.js
@@ -1,63 +1,26 @@
-import axios from 'axios'
-const {proto, generateWAMessageFromContent, prepareWAMessageMedia, generateWAMessageContent, getDevice} = (await import("@whiskeysockets/baileys")).default
+import fetch from 'node-fetch'
 
-let handler = async (message, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(message.chat, '🍭 Ingrese su petición de tiktok.', message, rcanal)
-async function createVideoMessage(url) {
-const { videoMessage } = await generateWAMessageContent({ video: { url } }, { upload: conn.waUploadToServer })
-return videoMessage
-}
-async function shuffleArray(array) {
-for (let i = array.length - 1; i > 0; i--) {
-const j = Math.floor(Math.random() * (i + 1));
-[array[i], array[j]] = [array[j], array[i]]
-}
-}
+let handler = async (m, { conn, text }) => {
+if (!text) return m.reply(`Ingresa una petición`)
+
 try {
-/*await message.react(rwait)
-conn.reply(message.chat, '🚩 *Descargando Su Video...*', message, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})*/
-let results = []
-let { data: response } = await axios.get('https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=' + text)
-let searchResults = response.data
-shuffleArray(searchResults)
-let selectedResults = searchResults.splice(0, 7)
-for (let result of selectedResults) {
-results.push({
-body: proto.Message.InteractiveMessage.Body.fromObject({ text: null }),
-footer: proto.Message.InteractiveMessage.Footer.fromObject({ text: dev }),
-header: proto.Message.InteractiveMessage.Header.fromObject({
-title: '' + result.title,
-hasMediaAttachment: true,
-videoMessage: await createVideoMessage(result.nowm)
-}),
-nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({ buttons: [] })})}
-const responseMessage = generateWAMessageFromContent(message.chat, {
-viewOnceMessage: {
-message: {
-messageContextInfo: {
-deviceListMetadata: {},
-deviceListMetadataVersion: 2
-},
-interactiveMessage: proto.Message.InteractiveMessage.fromObject({
-body: proto.Message.InteractiveMessage.Body.create({ text: '🍭 Resultado de: ' + text }),
-footer: proto.Message.InteractiveMessage.Footer.create({ text: '🍿 Tiktok - Serachs' }),
-header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
-carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: [...results] })})}}
-}, { quoted: message })
-await message.react(done)
-await conn.relayMessage(message.chat, responseMessage.message, { messageId: responseMessage.key.id })
+let api = await fetch(`https://deliriussapi-oficial.vercel.app/search/applemusic?text=${encodeURIComponent(text)}`)
+let json = await api.json()
+let JT = 'Applemusic  -  Search'
+json.forEach((video, index) => {
+JT += `\n\n`
+JT += `*Nro* : ${index + 1}\n`
+JT += `*Título* : ${video.title}\n`
+JT += `*Tipo* : ${video.type}\n`
+JT += `*Artista* : ${video.artists}\n`
+JT += `*Url* : ${video.url}\n`
+})
+
+await conn.sendFile(m.chat, json[0].image, 'hasumiBotFreeCodes.jpg', JT, m);
 } catch (error) {
-await conn.reply(message.chat, error.toString(), message)
+console.error(error)
 }}
 
-handler.help = ['tiktoksearch <txt>']
-handler.cookies = 1
-handler.register = true
-handler.tags = ['buscador']
-handler.command = ['tiktoksearch', 'tiktoks']
+handler.command = /^(applemusicsearch)$/i
+
 export default handler
