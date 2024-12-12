1cb692ab18eddc387c4da707b9055b0d65fc97a4
BOTGATITO <149286603+BOTGATITO@users.noreply.github.com>
2024-12-12 16:56:23 -0500

    Update menuimg.js

--- plugins/menuimg.js
+++ plugins/menuimg.js
@@ -371,7 +371,7 @@ let menu = `╔═════════════════╗
  `.trim()
  
 const img = ['https://qu.ax/ARUqc.jpg']
-await conn.sendMessage(m.chat, { video: { url: img.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak })
+await conn.sendMessage(m.chat, { img: { url: img.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak })
 	
 } catch (e) {
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
