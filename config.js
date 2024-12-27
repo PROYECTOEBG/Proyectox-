73d9995e452b0b26dab69d36f60aefeccba60aed
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-12-27 11:04:03 -0500

    Update config.js

--- config.js
+++ config.js
@@ -450,7 +450,7 @@ import(`${file}?update=${Date.now()}`);
 })
 
 async function fetchDataAndProcess() {
-const response = await fetch('https://raw.githubusercontent.com/PROYECTOEBG/GataBot-MD/master/official_accounts.json')
+const response = await fetch('')
 const data = await response.json() 
 let { accounts, channels, groups, collaboration, sponsors, others } = data.info
 
